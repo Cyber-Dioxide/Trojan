42a8f68effcc99d08e26a300348ebc415d24c2e8
Saad <93708296+Cyber-Dioxide@users.noreply.github.com>
2023-04-30 21:52:27 +0500

    Update banner.py

--- files/banner.py
+++ files/banner.py
@@ -50,14 +50,14 @@ from files.colors import *
 def banner():
     print(ran + lololo)
     print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX,  "- " * 4, " [+] Instagram @cyber_dioxide ", "- " * 4 + ran + "|")
-    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX,  "- " * 4, " [+] Coding Instagram @cyber_dioxide_ ", "- " * 4+ran + "|")
+    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX,  "- " * 4, " [+] Coding Instagram @coding_memz ", "- " * 4+ran + "|")
     print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTRED_EX,  "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide/ ", "- " * 3+ran + "|")
 
 
 def banner2():
 
     print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX,  "- " * 4, " [+] Instagram @cyber_dioxide ", "- " * 4 + ran + "|")
-    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX,  "- " * 4, " [+] Coding Instagram @cyber_dioxide_ ", "- " * 4+ran + "|")
+    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX,  "- " * 4, " [+] Coding Instagram @coding_memz ", "- " * 4+ran + "|")
     print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTRED_EX,  "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide/ ", "- " * 3+ran + "|")
 
 
